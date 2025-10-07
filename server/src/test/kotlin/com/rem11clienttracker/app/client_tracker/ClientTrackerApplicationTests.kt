a189b000772e8b2091d4bd42c06a73f1f5a7a9c1
Julieta Dalla Pozza <dallapozzajulieta@gmail.com>
2025-10-07 11:26:04 +0200

    latest commit

--- server/src/test/kotlin/com/rem11clienttracker/app/client_tracker/ClientTrackerApplicationTests.kt
+++ server/src/test/kotlin/com/rem11clienttracker/app/client_tracker/ClientTrackerApplicationTests.kt
@@ -1,12 +1,12 @@
+
+//kotlin.test library 
 package com.rem11clienttracker.app.client_tracker
-
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class ClientTrackerApplicationTests {
 
 	@Test
+	//test is an annotation (a label for testing functions)
 	fun contextLoads() {
 	}
 
